17aa834c1a04fdda9d98c1363935782bb148ad7d
Sharanu Mesta <sharanumesta1201@gmail.com>
2025-07-11 23:31:03 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -3,14 +3,23 @@ import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://leaderboard-sharanu.netlify.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://leaderboard-sharanu.netlify.app/",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+
 app.use(express.json());
 
 import userRouter from "./routes/user.router.js";
